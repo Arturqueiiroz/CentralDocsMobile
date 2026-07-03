092928142d6e5280e2440b9c2660a9d0e17e176c
nicolayneves7 <nicolayneves7@gmail.com>
2026-07-03 14:12:41 -0300

    Adiciona a tela "Sobre Nós" e atualiza a navegação inicial para essa tela.

--- App.tsx
+++ App.tsx
@@ -13,6 +13,7 @@ import DocumentosScreen from "./src/Presentation/views/Documentos/Documentos";
 import PerfilScreen from "./src/Presentation/views/Perfil/Perfil";
 import ConfiguracoesScreen from "./src/Presentation/views/Configuração/Configuracao";
 import LerDocumentoScreen from "./src/Presentation/views/QRcode/Qrcode";
+import SobreNosScreen from "./src/Presentation/views/SobreNos/Sobrenos";
 // Definição dos tipos das rotas para o TypeScript
 export type RootStackParamList = {
     Login: undefined;
@@ -31,7 +32,7 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator
-                initialRouteName="QRcode"
+                initialRouteName="SobreNos"
                 screenOptions={{
                     headerShown: false,
                 }}
@@ -79,6 +80,10 @@ export default function App() {
                 <Stack.Screen
                     name="QRcode"
                     component={LerDocumentoScreen}
+                />
+                <Stack.Screen
+                    name="SobreNos"
+                    component={SobreNosScreen}
                 />
 
 
