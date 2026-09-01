1c6ea19d4944b40863ea8fb428e9c5093f329b13
ARTUR COSMO DE QUEIROZ <artur.cqueiroz@senacsp.edu.br>
2026-09-01 13:59:01 -0300

    Adicionando tela de Esqueceu Senha com estrutura básica e integração ao estilo

---
+++ src/Presentation/views/EsqueceuSenha/EsqueceuSenha.tsx
@@ -0,0 +1,11 @@
+import { View, Text, TextInput, ScrollView } from "react-native";
+import styles from "../../theme/EsqueceuSenhaCss";
+export default function EsqueceuSenha () {
+    return (
+        <View style={styles.container}>
+            <ScrollView>
+
+            </ScrollView>
+        </View>
+    )
+}
